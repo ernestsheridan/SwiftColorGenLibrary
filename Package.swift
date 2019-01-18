8efe8fe2e9b9b54ffc7f1474b97b38e43cb8ac44
tvlc <tomas.vlcek@neoware.cz>
2019-01-18 21:58:48 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/tadija/AEXML.git", from: "4.2.2"),
-        .package(url: "https://github.com/silt-lang/CommandLine", from: "4.0.0")
+        .package(url: "https://github.com/jatoben/CommandLine", from: "4.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
